34bc474292e161b7063fe4fc1103ee0103c029ab
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2026-03-08 20:55:23 -0300

    create component Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
  import './styles/theme.css'
  import './styles/global.css';
-import { Heading } from './components/Heanding';
-import {Timer} from 'lucide-react';
-
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
  export function App(){
   return (
-    <div>
-      <Heading>
-        Ola Mundo1 !
-        <button>
-          <Timer />
-        </button>
-      </Heading>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Reprehenderit, cumque nulla quae eaque quo quia omnis 
-        placeat ullam nobis harum saepe alias corrupti dolores unde 
-        explicabo vel cum minima laboriosam.</p>
-    </div>
+    <>
+      <Container>
+        <Heading>Logo</Heading>
+      </Container>
+
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
+    </>
   );
 }
 
