4ee5bd25df015ebff496c411cfc6b1c35881b65a
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2026-03-13 20:31:49 -0300

    feat: create UI component CountDown

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Logo } from './components/Logo';
 import './styles/theme.css'
 import './styles/global.css';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 
  export function App(){
   return (
@@ -14,6 +15,10 @@ import { Menu } from './components/Menu';
 
       <Container>
         <Menu/>
+      </Container>
+
+      <Container>
+        <CountDown/>
       </Container>
     </>
   );
