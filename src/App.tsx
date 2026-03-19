af617d53d9e7b9764a2195fa2f898a8eaba20006
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2026-03-19 10:32:31 -0300

    feat: create component DefaultInput

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './styles/theme.css'
 import './styles/global.css';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 
  export function App(){
   return (
@@ -20,6 +21,36 @@ import { CountDown } from './components/CountDown';
       <Container>
         <CountDown/>
       </Container>
+
+      <Container>
+        <form  className='form'>
+          <div className='formRow'>
+            <DefaultInput 
+              type="text"
+              labelText="task:" 
+              id="inputTask"
+              placeholder='Digite o texto...' 
+              />
+          </div>
+
+          <div className='formRow'>
+            <p>Nesse ciclo foque por 25 min.</p>
+          </div>
+
+          <div className='formRow'>
+          <label htmlFor="">Ciclos:</label>
+            <span>
+              0 0 0 0 0 0 0 0 0 0
+            </span>
+          </div>
+          
+          <div className='formRow'>
+            <button>Iniciar</button>
+          </div>
+
+        </form>
+      </Container>
+
     </>
   );
 }
