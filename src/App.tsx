034f711185f6ee05a42fcebab3fcb4986ca97b60
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2026-03-30 22:43:56 -0300

    feat: create template for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,10 @@
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
-
+import { Home } from './pages/Home';
 import './styles/theme.css'
 import './styles/global.css';
-import { Menu } from './components/Menu';
-import { CountDown } from './components/CountDown';
-import { DefaultInput } from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { Footer } from './components/Footer';
-import { PlayCircleIcon } from 'lucide-react';
 
  export function App(){
   return (
-    <>
-      <Container>
-        <Logo/>
-      </Container>
-
-      <Container>
-        <Menu/>
-      </Container>
-
-      <Container>
-        <CountDown/>
-      </Container>
-
-      <Container>
-        <form  className='form'>
-          <div className='formRow'>
-            <DefaultInput 
-              type="text"
-              labelText="task:" 
-              id="inputTask"
-              placeholder='Digite o texto...' 
-              />
-          </div>
-
-          <div className='formRow'>
-            <p>Nesse ciclo foque por 25 min.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles/>
-          </div>
-          
-          <div className='formRow'>
-            <DefaultButton icon={< PlayCircleIcon />} />
-          </div>
-
-        </form>
-      </Container>
-
-      <Container>
-        <Footer/>
-      </Container>
-
-    </>
+    <Home/>
   );
 }
 
---
+++ src/components/MainForm/index.tsx
@@ -0,0 +1,32 @@
+import { PlayCircleIcon } from 'lucide-react';
+import { Cycles } from '../Cycles';
+import { DefaultButton } from '../DefaultButton';
+import { DefaultInput } from '../DefaultInput';
+
+export function MainForm(){
+    return (
+        <form  className='form'>
+          <div className='formRow'>
+            <DefaultInput 
+              type="text"
+              labelText="task:" 
+              id="inputTask"
+              placeholder='Digite o texto...' 
+              />
+          </div>
+
+          <div className='formRow'>
+            <p>Nesse ciclo foque por 25 min.</p>
+          </div>
+
+          <div className='formRow'>
+            <Cycles/>
+          </div>
+          
+          <div className='formRow'>
+            <DefaultButton icon={< PlayCircleIcon />} />
+          </div>
+
+        </form>
+    );
+}
