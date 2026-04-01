e66867d77fb9ed025dc1523e6189b5e767725311
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2026-03-31 22:05:00 -0300

    feat: create component GenericHtml and page structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Home } from './pages/Home';
+
 import './styles/theme.css'
 import './styles/global.css';
+import { NotFound } from './pages/Notfound';
+
 
  export function App(){
   return (
-    <Home/>
+   <NotFound/>
   );
 }
 
